99b696ceab12279fba760109c4418b59ff2381c9
Visal Rajapakse <visalrajapakse@gmail.com>
2021-03-03 19:28:48 +0530

    initial commit

---
+++ AnimatingLabelOverlay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimatingLabelOverlay
+//
+//  Created by Visal Rajapakse on 2021-03-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
